9ab870f97d9d1f3be2f287e28d76c79be0435c1d
fernando duenas <fernando39615@aim.com>
2020-12-20 12:24:21 -0500

    another commit

--- js/app.js
+++ js/app.js
@@ -5,18 +5,19 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('button');
 const todoList = document.querySelector('.todo-list');
 
-/*******************
- * EVENT LISTENERS *
- *******************/
-todoButton.addEventListener('click', addTodo);
-
 /*************
  * FUNCTIONS *
  *************/
 const addTodo = (event) => {
     event.preventDefault();
-    console.log(todoInput.value);
+    // console.log(todoInput.value);
     const li = document.createElement('li');
     li.innerText = todoInput.value;
+    // li.innerText = 'hello there!!';
     todoList.appendChild(li);
 }
+
+/*******************
+ * EVENT LISTENERS *
+ *******************/
+todoButton.addEventListener('click', addTodo);
