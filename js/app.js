d013ecdf03690278832616c4b48bdf802a51c4b9
fernando duenas <fernando39615@aim.com>
2020-12-20 20:23:20 -0500

    adding a todo list array

--- js/app.js
+++ js/app.js
@@ -8,16 +8,87 @@ const todoList = document.querySelector('.todo-list');
 /*************
  * FUNCTIONS *
  *************/
-const addTodo = (event) => {
-    event.preventDefault();
-    // console.log(todoInput.value);
-    const li = document.createElement('li');
-    li.innerText = todoInput.value;
-    // li.innerText = 'hello there!!';
-    todoList.appendChild(li);
+const addTodo = (todo) => {
+    todos.push(todo);
+}
+
+const printTodo = (todo) => {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todo.text;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa fa-check"></i>'
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa fa-trash"></i>'
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    
+    todoList.appendChild(todoDiv);
+
+    if (todo.complete) {
+        newTodo.classList.add('complete')
+    }
+
+    newTodo.addEventListener('click', (event) => {
+        event.target.classList.toggle('complete')
+        const id = event.target.id;
+        todos[id].complete = !todos[id].complete;
+    })
+}
+
+const printTodos = function() {
+    for (const todo of todos) {
+    printTodo(todo);
+    }
+}
+
+printTodos();
+
+const deleteCheck = (event) => {
+    const item = event.target;
+    
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+const clearTodos = function() {
+    const todos = document.querySelectorAll('.todo-item');
+    for (const todo of todos) {
+        todo.remove();
+    }
 }
 
 /*******************
  * EVENT LISTENERS *
  *******************/
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    // const inputBox = document.querySelector('.todo-input');
+    const todo = {
+        text: todoInput.value,
+        priority: 2,
+        complete: false,
+        id: todos.length,
+    }
+
+    addTodo(todo);
+    printTodo(todo);
+    todoInput.value = '';
+})
+
+todoList.addEventListener('click', deleteCheck);
---
+++ js/todos.js
@@ -0,0 +1,43 @@
+let todos = [
+{
+    text: 'milk',
+    complete: false,
+    priority: 1,
+    id: 0,
+},
+
+{
+    text: `bread`,
+    complete: true,
+    priority: 2,
+    id: 1,
+},
+
+{
+    text: 'cheese',
+    complete: false,
+    priority: 2,
+    id: 2,
+},
+
+{
+    text: 'potatoes',
+    complete: false,
+    priority: 1,
+    id: 3,
+},
+
+{
+    text: 'pasta',
+    complete: true,
+    priority: 2,
+    id: 4,
+},
+
+{
+    text: 'bananas',
+    complete: true,
+    priority: 1,
+    id: 5,
+},
+]
