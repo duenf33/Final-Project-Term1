5e6fbffcf230c039b45be56ae64e8356fe1cec6d
fernando duenas <fernando39615@aim.com>
2020-12-20 23:46:39 -0500

    re-adjusting the Do List

--- js/app.js
+++ js/app.js
@@ -13,25 +13,10 @@ const addTodo = (todo) => {
 }
 
 const printTodo = (todo) => {
-    // const todoDiv = document.createElement('div');
-    // todoDiv.classList.add('todo');
-    
     const newTodo = document.createElement('li');
     newTodo.innerText = todo.text;
     newTodo.classList.add('todo-item');
     todoList.appendChild(newTodo);
-
-    // const completedButton = document.createElement('button');
-    // completedButton.innerHTML = '<i class="fa fa-check"></i>'
-    // completedButton.classList.add('complete-btn');
-    // todoDiv.appendChild(completedButton);
-    
-    // const trashButton = document.createElement('button');
-    // trashButton.innerHTML = '<i class="fa fa-trash"></i>'
-    // trashButton.classList.add('trash-btn');
-    // todoDiv.appendChild(trashButton);
-    
-    // todoList.appendChild(todoDiv);
 
     newTodo.id = todo.id;
 
@@ -53,20 +38,6 @@ const printTodos = function() {
 }
 
 printTodos();
-
-// const deleteCheck = (event) => {
-//     const item = event.target;
-    
-//     if(item.classList[0] === 'trash-btn'){
-//         const todo = item.parentElement;
-//         todo.remove();
-//     }
-
-    // if(item.classList[0] === 'complete-btn'){
-    //     const todo = item.parentElement;
-    //     todo.classList.toggle('completed');
-    // }
-// }
 
 const clearTodos = function() {
     const todos = document.querySelectorAll('.todo-item');
@@ -91,5 +62,3 @@ todoButton.addEventListener('click', (event) => {
     printTodo(todo);
     todoInput.value = '';
 })
-
-// todoList.addEventListener('click', deleteCheck);
